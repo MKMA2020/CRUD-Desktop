c0c16e298f6beb8a926774da976fe2dd9a701164
Martin Valiente Ainz <2dam@MACINF01.TartangaLH.eus>
2021-01-19 12:25:54 +0100

    dev_newIngredient created.

--- src/security/Ciphering.java
+++ src/security/Ciphering.java
@@ -16,8 +16,6 @@ import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import javax.crypto.Cipher;
-import javax.xml.bind.DatatypeConverter;
-import org.apache.commons.codec.binary.Base32;
 
 /**
  * Methods to encrypt and decrypt
@@ -25,12 +23,12 @@ import org.apache.commons.codec.binary.Base32;
  * @author Martin Gros
  */
 public class Ciphering {
-    
+
     public byte[] getPublicFileKey() throws IOException {
 
         InputStream keyfis = Ciphering.class.getClassLoader()
                 .getResourceAsStream("security/Public.key");
-       
+
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         int len;
@@ -42,12 +40,10 @@ public class Ciphering {
         keyfis.close();
         return os.toByteArray();
     }
-    
 
     public String cifrarTexto(String mensaje) {
         byte[] encodedMessage = null;
-        Base32 base = new Base32();
-        String encodedHexMessage="";
+        String encodedHexMessage = "";
         try {
             // Public Key loaded from a relative path (keys are placed inside the project)
             byte fileKey[] = getPublicFileKey();
@@ -63,62 +59,13 @@ public class Ciphering {
             encodedMessage = cipher.doFinal(mensaje.getBytes());
             //encodedMessage = base.encode(encodedMessage);
             encodedHexMessage = toHexadecimal(encodedMessage);
-            
-            
-            
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return encodedHexMessage;
     }
 
-    /**
-     * Descifra un texto con RSA, modo ECB y padding PKCS1Padding (asim�trica) y
-     * lo retorna
-     *
-     * @param mensaje El mensaje a descifrar
-     * @return El mensaje descifrado
-     */
-    public byte[] descifrarTexto(byte[] mensaje) {
-        byte[] decodedMessage = null;
-        Base32 base = new Base32();
-        try {
-            // Private Key
-            byte fileKey[] = fileReader(getClass().getResource("Private.key").getFile());
-            //getClass().getResource("c:\\\\claves\\\\Public.key");
-            System.out.println("SIZE:--> " + fileKey.length + " bytes");
-
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            PKCS8EncodedKeySpec pKCS8EncodedKeySpec = new PKCS8EncodedKeySpec(fileKey);
-            PrivateKey privateKey = keyFactory.generatePrivate(pKCS8EncodedKeySpec);
-
-            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            decodedMessage = cipher.doFinal(mensaje);
-            decodedMessage = base.decode(decodedMessage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return decodedMessage;
-    }
-
-    /**
-     * Retorna el contenido de un fichero
-     *
-     * @param path Path del fichero
-     * @return El texto del fichero
-     */
-    private byte[] fileReader(String path) {
-        byte ret[] = null;
-        File file = new File(path);
-        try {
-            ret = Files.readAllBytes(file.toPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ret;
-    }
     // Convierte Array de Bytes en hexadecimal
     private static String toHexadecimal(byte[] resumen) {
         String HEX = "";
