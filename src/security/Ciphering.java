1711dde6879c7978cccd85e328861b8c9a330d02
Martin Valiente Ainz <TINI@Portatil>
2021-01-19 20:39:24 +0100

    dev_SideMenu Created

--- src/security/Ciphering.java
+++ src/security/Ciphering.java
@@ -6,18 +6,12 @@
 package security;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.security.KeyFactory;
-import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import javax.crypto.Cipher;
-import javax.xml.bind.DatatypeConverter;
-import org.apache.commons.codec.binary.Base32;
 
 /**
  * Methods to encrypt and decrypt
@@ -46,7 +40,6 @@ public class Ciphering {
 
     public String cifrarTexto(String mensaje) {
         byte[] encodedMessage = null;
-        Base32 base = new Base32();
         String encodedHexMessage="";
         try {
             // Public Key loaded from a relative path (keys are placed inside the project)
@@ -73,52 +66,6 @@ public class Ciphering {
         return encodedHexMessage;
     }
 
-    /**
-     * Descifra un texto con RSA, modo ECB y padding PKCS1Padding (asim�trica) y
-     * lo retorna
-     *
-     * @param mensaje El mensaje a descifrar
-     * @return El mensaje descifrado
-     */
-    public byte[] descifrarTexto(byte[] mensaje) {
-        byte[] decodedMessage = null;
-        Base32 base = new Base32();
-        try {
-            // Private Key
-            byte fileKey[] = fileReader(getClass().getResource("Private.key").getFile());
-            //getClass().getResource("c:\\\\claves\\\\Public.key");
-            System.out.println("SIZE:--> " + fileKey.length + " bytes");
-
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            PKCS8EncodedKeySpec pKCS8EncodedKeySpec = new PKCS8EncodedKeySpec(fileKey);
-            PrivateKey privateKey = keyFactory.generatePrivate(pKCS8EncodedKeySpec);
-
-            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            decodedMessage = cipher.doFinal(mensaje);
-            decodedMessage = base.decode(decodedMessage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return decodedMessage;
-    }
-
-    /**
-     * Retorna el contenido de un fichero
-     *
-     * @param path Path del fichero
-     * @return El texto del fichero
-     */
-    private byte[] fileReader(String path) {
-        byte ret[] = null;
-        File file = new File(path);
-        try {
-            ret = Files.readAllBytes(file.toPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ret;
-    }
     // Convierte Array de Bytes en hexadecimal
     private static String toHexadecimal(byte[] resumen) {
         String HEX = "";
